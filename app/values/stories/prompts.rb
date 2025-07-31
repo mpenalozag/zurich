0e1a4c1ad35591a7d1cda69dea96f4302fdd8428
mpenalozag <mpenaloza@uc.cl>
2025-07-31 01:49:01 -0400

    feat: promp to get story with images

--- app/values/stories/prompts.rb
+++ app/values/stories/prompts.rb
@@ -7,6 +7,8 @@ module Stories
       content: <<~PROMPT,
         You are a story writer for a children's book. You are given a prompt requesting a story.
         You must identify all the characters in the story and return them in a JSON format.
+        Characters must be individual characters, for example, two cats should be two different characters,
+        each of them with a name.
         The JSON format must be:
         {
           "characters": [
@@ -16,6 +18,28 @@ module Stories
             }
           ]
         }
+        You mustn't include any other text in your response, only the JSON.
+      PROMPT
+    }, T::Hash[Symbol, String])
+    
+    GET_STORY_IN_CHAPTERS_WITH_1_IMAGE_PER_CHAPTER_SYSTEM_ROLE = T.let({
+      role: "system",
+      content: <<~PROMPT,
+        You are a story writer for a children's book. You are given a prompt requesting a story.
+        You are also given a JSON with the characters of the story and their descriptions.
+        You must write the story in chapters, with one description of an image per chapter.
+        The story should be written in the following format:
+        {
+          "chapters": [
+            {
+              "chapter": "string",
+              "image": "string"
+            }
+          ]
+        }
+        Each chapter must contain a piece of the story, at least 100 words.
+        The image in each chapter must be representative of the chapter.
+        The story in total should be at least 1000 words and less than 2000 words.
         You mustn't include any other text in your response, only the JSON.
       PROMPT
     }, T::Hash[Symbol, String])
