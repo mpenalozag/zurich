d881306ef521c75f309953905f7cba43b1e81739
mpenalozag <mpenaloza@uc.cl>
2025-08-02 01:09:54 -0400

    feat: Define production service

---
+++ app/services/storage/production_service.rb
@@ -0,0 +1,16 @@
+# typed: strict
+
+class Storage::ProductionService
+  extend T::Sig
+  include StorageService
+
+  sig { override.params(image: String, path: String).returns(String) }
+  def store_image(image, path)
+    raise "Not implemented"
+  end
+
+  sig { override.params(path: String).returns(T.nilable(String)) }
+  def retrieve_image(path)
+    raise "Not implemented"
+  end
+end
