c75d539cecaecef5ca9d64457b0d12218d205396
mpenalozag <mpenaloza@uc.cl>
2025-08-03 06:00:03 -0400

    feat: Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_30_034739) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_03_094828) do
+  create_table "chapters", force: :cascade do |t|
+    t.string "text_path"
+    t.string "image_path"
+    t.integer "story_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_chapters_on_story_id"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image_path"
+    t.integer "story_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_characters_on_story_id"
+  end
+
   create_table "stories", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "chapters", "stories"
+  add_foreign_key "characters", "stories"
 end
