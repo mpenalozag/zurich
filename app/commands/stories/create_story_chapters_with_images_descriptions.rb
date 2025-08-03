144a24b3e277257f4f663b9744c397c8409c35a9
mpenalozag <mpenaloza@uc.cl>
2025-08-03 07:32:45 -0400

    feat: Create story chapters with images descriptions

---
+++ app/commands/stories/create_story_chapters_with_images_descriptions.rb
@@ -0,0 +1,41 @@
+# typed: strict
+
+class Stories::CreateStoryChaptersWithImagesDescriptions < Command
+  extend T::Sig
+
+  sig { params(story_prompt: String, story: Story).void }
+  def initialize(story_prompt, story)
+    @story_prompt = story_prompt
+    @story = story
+  end
+
+  private
+
+  sig { void }
+  def execute
+    Rails.logger.info("Creating story chapters with images descriptions")
+    response = OpenAiService.get_story_splitted_by_chapters_and_images(@story_prompt, formatted_characters)
+    chapters = JSON.parse(response)["chapters"]
+    create_story_chapters(chapters)
+  end
+
+  sig { returns(T::Hash[Symbol, T::Array[T::Hash[Symbol, String]]]) }
+  def formatted_characters
+    {
+      "characters":
+        @story.characters.map do |character|
+          { "name": character.name, "description": character.description }
+        end
+    }
+  end
+
+  sig { params(chapters: T::Array[T::Hash[String, String]]).void }
+  def create_story_chapters(chapters)
+    chapters.each do |chapter|
+      chapter_text = T.must(chapter["chapter"])
+      image_description = T.must(chapter["image_description"])
+      text_path = Storage::StoreText.new(text: chapter_text).run
+      Chapter.create!(text_path: text_path, image_description: image_description, story: @story)
+    end
+  end
+end
---
+++ spec/commands/stories/create_story_chapters_with_images_description_spec.rb
@@ -0,0 +1,70 @@
+# typed: false
+
+require "rails_helper"
+
+RSpec.describe Stories::CreateStoryChaptersWithImagesDescriptions do
+  let(:story_prompt) { "A story about a big cat and his friend the mouse" }
+  let_it_be(:story) { create(:story, title: "Some story") }
+  let_it_be(:first_character) { create(:character, name: "big cat", description: "A big cat", story: story) }
+  let_it_be(:second_character) { create(:character, name: "mouse", description: "A mouse", story: story) }
+  let(:command) { described_class.new(story_prompt, story) }
+  let(:mock_openai_service) { double("OpenAiService") }
+
+  before do
+    stub_const("OpenAiService", mock_openai_service)
+    allow(Rails).to receive(:env).and_return("development")
+  end
+
+  def perform
+    command.run
+  end
+
+  describe '#execute' do
+    before do
+      allow(mock_openai_service).to receive(:get_story_splitted_by_chapters_and_images).and_return(
+        '{
+          "chapters": [
+            {
+              "chapter": "Chapter 1",
+              "image_description": "A big cat and his friend the mouse"
+            },
+            {
+              "chapter": "Chapter 2",
+              "image_description": "A big cat and his friend the mouse dancing in the park"
+            }
+          ]
+        }'
+      )
+      allow(Storage::StoreText).to receive(:new).and_return(double("Storage::StoreText", run: "path/to/text"))
+    end
+
+    let(:formatted_characters) do
+      {
+        "characters": [
+          { "name": "big cat", "description": "A big cat" },
+          { "name": "mouse", "description": "A mouse" }
+        ]
+      }
+    end
+
+    it 'calls the OpenAiService method to get the story splitted by chapters and images' do
+      perform
+      expect(mock_openai_service).to have_received(:get_story_splitted_by_chapters_and_images).with(story_prompt, formatted_characters)
+    end
+
+    it 'creates the story chapters with images descriptions' do
+      perform
+      expect(story.chapters.count).to eq(2)
+      expect(story.chapters.first.text_path).to eq("path/to/text")
+      expect(story.chapters.first.image_description).to eq("A big cat and his friend the mouse")
+      expect(story.chapters.second.text_path).to eq("path/to/text")
+      expect(story.chapters.second.image_description).to eq("A big cat and his friend the mouse dancing in the park")
+    end
+
+    it 'associates the story chapters to the story' do
+      perform
+      expect(story.chapters.first.story).to eq(story)
+      expect(story.chapters.second.story).to eq(story)
+    end
+  end
+end
